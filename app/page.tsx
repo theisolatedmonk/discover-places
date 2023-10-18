8fbcce1b37f633ca3ddbc105336aae4ece6da576
theisolatedmonk <96rahulseth@gmail.com>
2023-10-18 12:03:20 +0530

    result  component implimented

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,32 @@
-import Image from 'next/image'
+"use client"
 import Hero from './components/Hero'
-import category from './data/category'
+import PlaceList from './components/PlaceList'
+import Image from 'next/image'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
+
+  const [placeList, setPlaceList] = useState([])
+  useEffect(() => {
+    getPlaceList('Hotels in New York');
+
+  }, [])
+
+  const getPlaceList = async (value: string) => {
+    setPlaceList([]);
+    const result = await fetch("/api/google-place-api?q=" + value)
+    const data = await result.json();
+    console.log("data- ", data)
+    console.log("data.Data- ", data.Data.results)
+
+    // setPlaceList(data.resp.results);
+    setPlaceList(data.Data.results);
+  }
   return (
     <div>
-      <div className="text-center">
-        <Image src='/bg.png' alt={'hero-image'} width={800}
-          height={200} className='w-full absolute mt-[-50px]' />
-        <div className=' mt-[70px]'>
-          <h2 className="text-[55px] text-red-600 tracking-widest font-semibold">DISCOVER</h2>
-          <h2 className="text-[20px]"> Your Amazing City</h2>
-          <div className=' flex mt-5 z-10 gap-2 items-center justify-center'>
-            <input type="text" placeholder='Search Anything' className='z-20 bg-white p-3 border-[1px] rounded-full px-5 w-[36%] shadow-sm outline-red-300' />
-            <button className='bg-red-600 rounded-full p-3 shadow-sm z-10 cursor-pointer hover:scale-105 transition-all'>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-white ">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-              </svg>
+      <Hero userInput={(value: string) => getPlaceList(value)} />
 
-            </button>
-          </div>
-          <div className="mt-5 flex flex-col justify-center items-center">
-            <h2>
-              Or Browse the category
-            </h2>
-            <div className="grid grid-cols-3 md:grid-cols-7 w-[50%] gap-5 mt-3 justify-center">
-              {category.map((item, index) => (
-                <div key={index} className='border-[1px] w-[60px] p-4 bg-white rounded-full z-10 hover:border-red-600 hover:scale-110 cursor-pointer transition-all'>
-                  <Image src={item.icon} alt={item.name} width={30} height={30} />
-                </div>
-              ))}
-            </div>
-          </div>
-        </div>
-      </div>
-
+      {placeList ? <PlaceList placeList={placeList} /> : null}
     </div>
   )
 }
